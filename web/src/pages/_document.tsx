e3176532affed946d64c6d92750c9ff2d85f03a5
Yuanjing Xue <yuanjingxue1987@gmail.com>
2022-08-16 02:35:41 -0700

    disable zoom in mobile browser

---
+++ web/src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class HtmlDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+         </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
